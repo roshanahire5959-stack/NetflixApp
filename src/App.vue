3a285912007f38287cf5b257303f0d772fb1cf66
Roshan Ahire <roshanahire5959@gmail.com>
2026-06-03 12:46:43 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-8">
+    <h1 class="text-4xl font-bold text-blue-500">
+      Tailwind Working
+    </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
